38a73aabf9e25c427c8af015edf242431ecc1253
David Green <djoeyg7@gmail.com>
2022-02-17 11:40:02 -0800

    Added search filters for Orders & Purchases

--- src/pages/Orders.js
+++ src/pages/Orders.js
@@ -21,6 +21,11 @@ function Orders({ setOrderToEdit }) {
         <h2>Manage Orders Data</h2>
         <div className="App-header">
         <Link className="App-link" to="/order-tracks">Add new Order</Link>
+        <p>Filter Orders by Customer ID#</p>
+        <span>
+          <input type="text" placeholder="Customer ID#" />   
+          <button onClick={e => e.preventDefault()}>Search</button>
+        </span>
         <br></br>
         <AllOrdersList ordersInfo={allOrders} onOrderUpdate={onOrderUpdate}></AllOrdersList>
         <br></br>
--- src/pages/Purchases.js
+++ src/pages/Purchases.js
@@ -21,6 +21,12 @@ function Purchases({ setPurchaseToEdit }) {
       <h2>Manage Purchases Data</h2>
         <div className="App-header">
         <br></br>
+        <p>Filter Purchases by Customer ID#</p>
+        <span>
+          <input type="text" placeholder="Customer ID#" />   
+          <button onClick={e => e.preventDefault()}>Search</button>
+        </span>
+        <br></br>
         <AllPurchasesList purchasesInfo={allPurchases} onPurchaseUpdate={onPurchaseUpdate}></AllPurchasesList>
         <br></br>
         </div>
