bf8c4631d19cd476a94eff0e3bd6a95d12701cfc
David Green <djoeyg7@gmail.com>
2022-03-09 11:01:28 -0800

    Completed Order form insert functionality

--- src/pages/OrderForm.js
+++ src/pages/OrderForm.js
@@ -1,59 +1,119 @@
 import '../App.css';
 import NavBar from '../components/navBarLinks';
-import React from 'react';
+import moment from 'moment';
+import React, { useState } from 'react';
 
 function OrderForm() {
-  return (
-    <>
-    <NavBar></NavBar>
-    <div className="body">
-        <h2>Order &amp; Purchase Tracks for Download</h2>
-            <div className="App-header">
-                <p>Make entries for each field below.</p>
-                <table className="table">
-                    <tbody>
-                        <tr>
-                            <td>First Name:</td>
-                            <td><input type="text" /></td>
-                        </tr>
-                        <tr>
-                            <td>Last Name:</td>
-                            <td><input type="text" /></td>
-                        </tr>
-                        <tr>
-                            <td>Track Title:</td>
-                            <td><input type="text" /></td>
-                        </tr>
-                        <tr>
-                            <td>Retail Price:</td>
-                            <td><input type="text" /></td>
-                        </tr>
-                        <tr>
-                            <td>Date &amp; Time:</td>
-                            <td><input type="text" /></td>
-                        </tr>
-                        <tr>
-                            <td>Credit Card Number:</td>
-                            <td><input type="text" /></td>
-                        </tr>
-                        <tr>
-                            <td>Credit Card Exp Date:</td>
-                            <td><input type="text" /></td>
-                        </tr>
-                    </tbody>
-                </table>
-                <br></br>
-                <button className='button'
-                    onClick={e => {
-                  alert('Track has been purchased.\nOrder will be completed momentarily.');
-                  e.preventDefault();
-                  }}>Submit
-                </button>
-                <br></br>
-            </div>
-    </div>
-    </>
-  );
+
+    const [customerID, setCustomerID] = useState('');
+    const [orderDateTime, setOrderDateTime] = useState('');
+    const [purchaseDateTime, setPurchaseDateTime] = useState('');
+    const [orderComplete, setOrderComplete] = useState('');
+    const [totalPurchase, setTotalPurchase] = useState('');
+    const [creditCardNum, setCreditCardNum] = useState('');
+    const [creditCardExp, setCreditCardExp] = useState('');
+
+    const addOrder = async () => {
+        const newOrder = { customerID, orderDateTime, orderComplete };
+        const response = await fetch('/api/insert-order', {
+            method: 'POST',
+            body: JSON.stringify(newOrder),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        if (response.status === 200) {
+            alert("Successfully added the new order information");
+        } else {
+            alert(`Failed to add new order, status code = ${response.status}`);
+        }
+    };
+
+    const addPurchase = async () => {
+        const newPurchase = { customerID, purchaseDateTime, totalPurchase, creditCardNum, creditCardExp};
+        const response = await fetch('/api/insert-purchase', {
+            method: 'POST',
+            body: JSON.stringify(newPurchase),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        if (response.status === 200) {
+            alert("Successfully added the new purchase information");
+        } else {
+            alert(`Failed to add new purchase, status code = ${response.status}`);
+        }
+    };
+
+    const orderSubmit = async () => {
+
+        // Gets the current date & time
+        // https://zetcode.com/javascript/momentjs/
+        let currentDateTime = moment().format("YYYY-MM-DD HH:mm:ss");
+        console.log(currentDateTime);
+        setOrderDateTime(currentDateTime);
+        setPurchaseDateTime(currentDateTime);
+        setOrderComplete(1);
+        addOrder();
+        addPurchase();
+    };
+
+    return (
+        <>
+        <NavBar></NavBar>
+            <div className="body">
+                <h2>Order &amp; Purchase Tracks for Download</h2>
+                    <div className="App-header">
+                        <p>Make entries for each field below.</p>
+                        <table className="table">
+                            <tbody>
+                                <tr>
+                                    <td>Customer ID#:</td>
+                                    <td>
+                                        <input type="text" 
+                                        value={customerID}
+                                        placeholder="ID value"
+                                        onChange={e => setCustomerID(e.target.value)}/>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Total Purchase Amout:</td>
+                                    <td>
+                                        <input type="text" 
+                                        value={totalPurchase}
+                                        placeholder="Total Purchase"
+                                        onChange={e => setTotalPurchase(e.target.value)}/>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Credit Card Number:</td>
+                                    <td>
+                                        <input type="text" 
+                                        value={creditCardNum}
+                                        placeholder="16 digit CC number"
+                                        onChange={e => setCreditCardNum(e.target.value)}/>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Credit Card Exp Date:</td>
+                                    <td>
+                                        <input type="text"
+                                        value={creditCardExp} 
+                                        placeholder="MM / YYYY"
+                                        onChange={e => setCreditCardExp(e.target.value)}/>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <br></br>
+                        <button className='button'
+                            onClick={orderSubmit}>Submit
+                        </button>
+                        <br></br>
+                    </div>
+                </div>
+        </>
+    );
 }
 
 export default OrderForm;
